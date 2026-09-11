db7c1f2945452d29801ee33a82ba8bb43000ade1
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-09-11 19:33:13 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG: единственное место, где меняются данные ================= */
 window.SITE = {
-  CA:      "SOON",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
+  CA:      "0x4229ef0173b3FFa25e2fe07A7A6AC1FCB771c2A0",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
   X_URL:   "https://x.com/whorefly_rh",
   NAME:    "Whore Fly",
   TICKER:  "$WHOREFLY",
